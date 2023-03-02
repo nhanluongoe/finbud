b26ee06634108b822497c7dc1c22a4459dfad143
nhanluongoe <nhanlt18@gmail.com>
2023-03-02 22:28:37 +0700

    chore: add useProfile hook

---
+++ src/components/hooks/useProfile.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { supabase } from '../../lib/initSupabase';
+
+async function getCurrentProfile() {
+  const session = await supabase.auth.getSession();
+  const userId = session.data.session?.user.id;
+
+  return supabase.from('profiles').select().eq('id', userId);
+}
+
+export default function useProfile() {
+  // TODO: handle to save this data to local storage for later use to save api call
+  const { data } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getCurrentProfile,
+    select: (data) => data.data,
+    staleTime: Infinity,
+  });
+
+  const profile = data?.[0];
+  const firstName = profile?.first_name ?? '';
+  const lastName = profile?.last_name ?? '';
+
+  return {
+    firstName,
+    lastName,
+  };
+}
