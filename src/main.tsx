7951021b9988ebf813df47ef1749b633f6e4538b
nhanluongoe <nhanlt18@gmail.com>
2023-03-02 22:27:01 +0700

    chore: init ReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 
 import { supabase } from './lib/initSupabase';
+import { queryClient } from './lib/initReactQuery';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SessionContextProvider supabaseClient={supabase}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </SessionContextProvider>
   </React.StrictMode>,
 );
