c2f2d7a01154267f083c0e76ba7490abf37fbbc6
nhanluongoe <nhanlt18@gmail.com>
2023-03-03 19:20:25 +0700

    refactor: move helpers

--- src/helper/account.ts
+++ src/helper/account.ts
@@ -1,5 +1,12 @@
-import { supabase } from '../../lib/initSupabase';
-import { Account } from '../../types';
+import { supabase } from '../lib/initSupabase';
+import { Account } from '../types';
+
+export async function fetchAccounts() {
+  const session = await supabase.auth.getSession();
+  const userId = session.data.session?.user.id;
+
+  return await supabase.from('accounts').select().eq('user_id', userId);
+}
 
 export async function addAccount(account: Omit<Account['Insert'], 'user_id'>) {
   const session = await supabase.auth.getSession();
@@ -17,11 +24,6 @@ export async function addAccount(account: Omit<Account['Insert'], 'user_id'>) {
 }
 
 export async function deleteAccount(account: Account['Update']) {
-  const accountName = account.name;
-
-  if (!accountName) {
-    throw new Error("User doesn't exist ");
-  }
-
-  await supabase.from('accounts').delete().eq('name', accountName);
+  return await supabase.from('accounts').delete().eq('id', account.id);
 }
+
