c64f8a3d55567d530617bad76eb2ce69ec28714f
nhanluongoe <nhanlt18@gmail.com>
2023-03-09 22:28:00 +0700

    feat: style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,54 @@
 import useProfile from '../../hooks/useProfile';
-import { supabase } from '../../lib/initSupabase';
+import {
+  FaRegDizzy,
+  FaRegGrinBeamSweat,
+  FaRegGrinBeam,
+  FaRegGrinHearts,
+  FaRegGrinSquintTears,
+  FaRegGrinSquint,
+  FaRegGrinStars,
+  FaRegGrinTears,
+  FaRegGrinTongueSquint,
+  FaRegGrinTongueWink,
+  FaRegGrinTongue,
+  FaRegGrinWink,
+  FaRegGrin,
+  FaRegGrimace,
+  FaRegGrinAlt,
+} from 'react-icons/fa';
+import { IconType } from 'react-icons';
+
+const faceIcons: IconType[] = [
+  FaRegGrimace,
+  FaRegDizzy,
+  FaRegGrinAlt,
+  FaRegGrinBeamSweat,
+  FaRegGrinBeam,
+  FaRegGrinHearts,
+  FaRegGrinSquintTears,
+  FaRegGrinSquint,
+  FaRegGrinStars,
+  FaRegGrinTears,
+  FaRegGrinTongueSquint,
+  FaRegGrinTongueWink,
+  FaRegGrinTongue,
+  FaRegGrinWink,
+  FaRegGrin,
+];
 
 export default function Header() {
   const { firstName, lastName } = useProfile();
 
+  const randomIconIndex = Math.floor(Math.random() * (faceIcons.length - 1));
+  const Icon = faceIcons[randomIconIndex];
+
   return (
-    <header>
-      <div>
-        <p>Hi {`${firstName} ${lastName}`}</p>
-        <button
-          onClick={async () => {
-            await supabase.auth.signOut();
-          }}
-        >
-          logout
-        </button>
+    <header className='my-5'>
+      <div className='flex items-center justify-end'>
+        <i className='mr-2'>
+          <Icon />
+        </i>
+        <p>{`${firstName} ${lastName}`}</p>
       </div>
     </header>
   );
