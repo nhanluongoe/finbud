7432a493584128625830cc1a2e4902a2927ceeeb
nhanluongoe <nhanlt18@gmail.com>
2023-03-10 14:47:02 +0700

    chore: remove unused icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,46 +1,7 @@
 import useProfile from '../../hooks/useProfile';
-import {
-  FaRegDizzy,
-  FaRegGrinBeamSweat,
-  FaRegGrinBeam,
-  FaRegGrinHearts,
-  FaRegGrinSquintTears,
-  FaRegGrinSquint,
-  FaRegGrinStars,
-  FaRegGrinTears,
-  FaRegGrinTongueSquint,
-  FaRegGrinTongueWink,
-  FaRegGrinTongue,
-  FaRegGrinWink,
-  FaRegGrin,
-  FaRegGrimace,
-  FaRegGrinAlt,
-} from 'react-icons/fa';
-import { IconType } from 'react-icons';
-
-const faceIcons: IconType[] = [
-  FaRegGrimace,
-  FaRegDizzy,
-  FaRegGrinAlt,
-  FaRegGrinBeamSweat,
-  FaRegGrinBeam,
-  FaRegGrinHearts,
-  FaRegGrinSquintTears,
-  FaRegGrinSquint,
-  FaRegGrinStars,
-  FaRegGrinTears,
-  FaRegGrinTongueSquint,
-  FaRegGrinTongueWink,
-  FaRegGrinTongue,
-  FaRegGrinWink,
-  FaRegGrin,
-];
 
 export default function Header() {
   const { firstName, lastName } = useProfile();
-
-  const randomIconIndex = Math.floor(Math.random() * (faceIcons.length - 1));
-  const Icon = faceIcons[randomIconIndex];
 
   return (
     <header className='mb-6'>
