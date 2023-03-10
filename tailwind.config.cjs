8b3128e03de39a370a91ddfcb197b7de9c8cfc90
nhanluongoe <nhanlt18@gmail.com>
2023-03-10 11:10:37 +0700

    feat: use Roboto as primary font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto'],
+      },
       colors: {
         backdrop: 'rgba(28,33,40,0.8)',
         canvas: colors.neutral,
