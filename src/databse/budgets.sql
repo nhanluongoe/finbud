8d632f711bf5d466614e757fa419831470fc9c15
nhanluongoe <nhanlt18@gmail.com>
2023-03-10 19:32:56 +0700

    chore: add script to create budget table

---
+++ src/databse/budgets.sql
@@ -0,0 +1,28 @@
+create table public.budgets (
+  id serial not null,
+  amount numeric default 0,
+  remaining numeric default 0,
+  created_at date default now(),
+
+  primary key (id);
+)
+
+alter table public.budgets enable row level security;
+
+create policy "Public budgets are viewable by authenticated users"
+on public.budgets for select
+to authenticated
+using (
+  true
+);
+
+
+create policy "Authenticated users can update their budgets"
+on public.budgets for update using (
+  auth.role() = 'authenticated'
+);
+
+create policy "Enable insert for authenticated users only" on public.budgets for insert with check (auth.role() = 'authenticated');
+
+create policy "Enable delete for authenticated users only" on public.budgets for delete to authenticated using (true);
+
