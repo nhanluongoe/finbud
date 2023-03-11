307e46e9910976bfc35078b562f3a0d132e9472d
nhanluongoe <nhanlt18@gmail.com>
2023-03-11 13:18:15 +0700

    fix: get only budgets for the current month

--- src/helper/budget.ts
+++ src/helper/budget.ts
@@ -2,7 +2,19 @@ import { supabase } from '../lib/initSupabase';
 import { Budget } from '../types';
 
 export async function fetchBudgets() {
-  return await supabase.from('budgets').select().order('id', { ascending: true });
+  const now = new Date();
+  const currentMonth = now.getMonth() + 1;
+  const currentYear = now.getFullYear();
+
+  const startDate = `${currentYear}-${currentMonth}-01T00:00:00.000Z`;
+  const endDate = `${currentYear}-${currentMonth + 1}-01T00:00:00.000Z`;
+
+  return await supabase
+    .from('budgets')
+    .select()
+    .gte('created_at', startDate)
+    .lte('created_at', endDate)
+    .order('id', { ascending: true });
 }
 
 export async function addBudget(budget: Omit<Budget['Insert'], 'remaining, created_at'>) {
