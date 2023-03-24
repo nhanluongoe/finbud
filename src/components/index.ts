15db3dd87000f07fe207b637c6476a41d84a5319
nhanluongoe <nhanlt18@gmail.com>
2023-03-24 19:17:54 +0700

    chore: export at index

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,6 @@ export { default as Header } from './Header/';
 export { default as Footer } from './Footer/';
 export { default as CommandLine } from './CommandLine/';
 export { default as Pagination } from './Pagination/';
+export { default as Transactions } from './Transactions/';
+export { default as Budgets } from './Budgets/';
+export { default as Empty } from './Empty/';
