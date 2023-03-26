7a12f51ed76c4264a2246dcb75c5be6ca6c491fe
nhanluongoe <nhanlt18@gmail.com>
2023-03-26 14:41:08 +0700

    feat: use react-router to manage routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,32 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { supabase } from './lib/initSupabase';
 import { queryClient } from './lib/initReactQuery';
 import ErrorProvider from './context/ErrorContext';
 import CommandProvider from './context/CommandContext';
+import Home from './pages/Home';
 
 import './styles/index.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -17,7 +35,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <QueryClientProvider client={queryClient}>
         <CommandProvider>
           <ErrorProvider>
-            <App />
+            <RouterProvider router={router} />
           </ErrorProvider>
         </CommandProvider>
       </QueryClientProvider>
